437e607723fbd05d813ec7f74b1a931b4535a308
spreka <hunreka93@hotmail.com>
2021-11-11 13:02:24 +0100

    test widget renamed

--- src/napari_annotatorj/_tests/test_dock_widget.py
+++ src/napari_annotatorj/_tests/test_dock_widget.py
@@ -4,7 +4,7 @@ import pytest
 # this is your plugin name declared in your napari.plugins entry point
 MY_PLUGIN_NAME = "napari-annotatorj"
 # the name of your widget(s)
-MY_WIDGET_NAMES = ["Example Q Widget", "example_magic_widget"]
+MY_WIDGET_NAMES = ["AnnotatorJ"]
 
 
 @pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
